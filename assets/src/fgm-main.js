c2e99589e999ef3435ecffde8e9c040358cb169a
Flávio Sousa <flavio.jsousa@gmail.com>
2015-06-01 23:16:05 +0100

    content to fgm.jade

--- assets/src/fgm-main.js
+++ assets/src/fgm-main.js
@@ -1,6 +1,6 @@
-//            //N   NE   E  SE  S  SW   W  NW   a   b   c   d   e  f   g  h 
-var ncols = [ 0,   1,  1,  1, 0, -1, -1, -1]//, -1,  1, -2,  2, -2, 2, -1, 1];
-var nrows = [ -1, -1,  0,  1, 1,  1,  0, -1]//, -2, -2, -1, -1,  1, 1,  2, 2];
+//            //N   NE   E  SE  S  SW   W  NW
+var ncols = [ 0,   1,  1,  1, 0, -1, -1, -1];
+var nrows = [ -1, -1,  0,  1, 1,  1,  0, -1];
 
 var cols = 20;
 var rows = 20;
@@ -61,39 +61,39 @@ function calcDist(ncol, nrow){
 ////////////////////
 (function () {
   var rags;
-  var ignmap = initmap(cols, rows, Infinity);
+  var ignitionMap = initmap(cols, rows, Infinity);
   var tn = t = 0;
 
   $(document).ready(function () {
-    rags = new MapRags('fgm-serial', ignmap, rows, cols, {max: 20, min: 0}); 
+    rags = new MapRags('fgm-serial', ignitionMap, rows, cols, {max: 20, min: 0}); 
     rags.render();
   });
 
-  function nextIttSerial(){
+  function dumbSpatialLoop(){
 
     t = tn;
     tn = Infinity;
 
 
+    //Spatial loop that looks for active cells, ie, cells with 
+    //ignition time = t
     for ( row = 0; row < rows; row++){
       for ( col = 0; col < cols; col++){
         var idx = col + cols*row;
         
-        //If the cell burns only in the future, skips and update tn if necessary
-        //finds the minimum tn from the cell's ignition times
-        if ( ignmap[idx] > t && tn > ignmap[idx] ){
+        //Update tn, so that tn is the minimum ignition time for all cells,
+        //in a given iteration
+        if ( ignitionMap[idx] > t && tn > ignitionMap[idx] ){
 
-          tn = ignmap[idx];
+          tn = ignitionMap[idx];
           continue;
         } 
 
-
-        //skip if cell burn time differs from current time
-        if ( ignmap[idx] !== t )
+        //skips cells that already burned
+        if ( ignitionMap[idx] !== t )
           continue;
 
-
-        //Neighbour loop if map[cell] = t
+        //propagate fire for all 8 neighours
         for (var n = 0; n < 8; n++){
 
           //neighbour index calc
@@ -106,15 +106,17 @@ function calcDist(ncol, nrow){
             continue;
 
           // skip if cell has already burned
-          if ( ignmap[nidx] < t )
+          if ( ignitionMap[nidx] < t )
             continue;
 
+          //Compute neighbour cell ignition time, based on the propagation speed
+          // tcell = current_time + cell distance / flame_speed
           igntime = t + ndist[n] / rosmap[idx];
 
 
-          //Update ignition time
-          if(igntime < ignmap[nidx]){
-            ignmap[nidx] = igntime;
+          //Update ignition time in the map only if the the current time is smaller
+          if(igntime < ignitionMap[nidx]){
+            ignitionMap[nidx] = igntime;
           }
 
           //Update tn
@@ -130,10 +132,10 @@ function calcDist(ncol, nrow){
   (function call(){
     setTimeout(function () {
 
-      nextIttSerial();
+      dumbSpatialLoop();
       if (itt++ % 1 === 0){
 
-        rags.updateMap(ignmap);
+        rags.updateMap(ignitionMap);
         rags.render();      
       }
 
@@ -142,7 +144,7 @@ function calcDist(ncol, nrow){
       
       call();
       
-    }, 500);  
+    }, 100);  
   })();
 })();
 
@@ -151,24 +153,24 @@ function calcDist(ncol, nrow){
 ////////////////////
 var mp;
 (function () {
-  var ignmap = initmap(cols, rows, 5);
-  mp = ignmap;
+  var ignitionMap = initmap(cols, rows, 5);
+  mp = ignitionMap;
   var monitor;
 
-  var rags = new MapRags('fgm-parallel', ignmap, rows, cols, {max: 20, min: 0}); 
+  var rags = new MapRags('fgm-parallel', ignitionMap, rows, cols, {max: 20, min: 0}); 
 
   $(document).ready(function () {
     rags.render();
   });
 
-  function nextIttParallel(){
+  function smartSpatialLoop(){
 
     for ( row = 0; row < rows; row++){
       for ( col = 0; col < cols; col++){
         var idx = col + cols*row;
 
         //skip ignition cell
-        if (ignmap[idx] === 0)
+        if (ignitionMap[idx] === 0)
           continue;
 
         var minArray = [];
@@ -183,14 +185,15 @@ var mp;
           if ( !(nrow >= 0 && nrow < rows && ncol >= 0 && ncol < cols))
             continue;
 
-          igntime = ignmap[nidx] + ndist[n] / rosmap[nidx];
+          // compute igniton time considering that the flame moves from the neighbour to the 
+          // center cell
+          var igntime = ignitionMap[nidx] + ndist[n] / rosmap[nidx];
           minArray.push(igntime);
 
         }
-
-        var min = Math.min.apply(null, minArray);
-
-        ignmap[idx] = min;
+        //associate the minimum of the propagation times to the ignition time 
+        // of the center cell
+        ignitionMap[idx] = Math.min.apply(null, minArray);;
 
       }
     }
@@ -201,17 +204,17 @@ var mp;
   (function call(){
     setTimeout(function () {
 
-      nextIttParallel();
+      smartSpatialLoop();
       if (itt++ % 1 === 0){
 
-        rags.updateMap(ignmap);
+        rags.updateMap(ignitionMap);
         rags.render();      
       }
 
-      if (monitor === ignmap[0])
+      if (monitor === ignitionMap[0])
         return;
 
-      monitor = ignmap[0];
+      monitor = ignitionMap[0];
       
       call();
       
